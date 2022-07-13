2c8f7088c566cf29b819dea17c09d461b1907324
Edilson <edilsonmorais16@gmail.com>
2022-07-12 21:11:00 -0300

    Alteração no script

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
-function insert(num){
+var resultado;
+
+function insert(num){      i
     var numero = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = numero + num
+    document.getElementById('resultado').innerHTML = numero + num        
 }
 
 function clean(){
@@ -8,15 +10,16 @@ function clean(){
 }
 
 function back(){
-    var resultado = document.getElementById('resultado').innerHTML;
+    resultado = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1);
 }
 
 function calcular(){
-    var resultado = document.getElementById('resultado').innerHTML;
+    resultado = document.getElementById('resultado').innerHTML;
+
     if(resultado) {
         document.getElementById('resultado').innerHTML = eval(resultado);
     } else {
         document.getElementById('resultado').innerHTML = "Nada";
-    }
+    }    
 }
